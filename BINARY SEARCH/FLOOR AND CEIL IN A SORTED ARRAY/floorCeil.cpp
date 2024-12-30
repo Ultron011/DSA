e6944a2c0e2ef959780fb62d16b7f1542ae21466
Ultron011 <ultroninfinity01@gmail.com>
2024-12-30 15:33:20 +0530

    TIME & SPACE COMPLEXITY OF FLOOR AND CEIL

--- BINARY SEARCH/FLOOR AND CEIL IN A SORTED ARRAY/floorCeil.cpp
+++ BINARY SEARCH/FLOOR AND CEIL IN A SORTED ARRAY/floorCeil.cpp
@@ -15,6 +15,14 @@
         The floor of 5 in the array is 4, and the ceiling of 5 in the array is 7.
 */
 
+/*
+    Time Complexity - 
+        O(log(n))
+    
+    Space Complexity - 
+        O(1)
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
