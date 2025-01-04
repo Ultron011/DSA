326f81223dff26decbaf6c9b7902e2cc2c8e8c65
Ultron011 <ultroninfinity01@gmail.com>
2025-01-04 18:57:37 +0530

    SEARCH IN A ROW AND COLUMN WISE SORTED MATRIX

---
+++ BINARY SEARCH/MEDIUM/SEARCH IN ROW AND COLUMN WISE SORTED MATRIX/BRUTE FORCE/searchRCsorted.cpp
@@ -0,0 +1,56 @@
+/**
+ * Problem - Search in a row-wise and column-wise sorted matrix.
+ *
+ * Description - Given an n x m matrix where each row is sorted in ascending order from left to right,
+ * and each column is sorted in ascending order from top to bottom, write a function that searches for a 
+ * target value in the matrix. The function should return true if the target is found, and false otherwise.
+ *
+ * Input :
+ * matrix = [
+ *   [1, 4, 7, 11, 15],
+ *   [2, 5, 8, 12, 19],
+ *   [3, 6, 9, 16, 22],
+ *   [10, 13, 14, 17, 24],
+ *   [18, 21, 23, 26, 30]
+ * ]
+ * target = 5
+ *
+ * Output :
+ * true
+ *
+ * Explanation The target value 5 is present in the matrix at position (1, 1).
+ *
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+    Time complexity - 
+        O(N X M), where N = given row number, M = given column number.
+
+    Space complexity - 
+        O(1)
+
+*/
+
+bool searchElement(vector<vector<int>> &matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    for  (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (matrix[i][j] == target) return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19},
+        {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24},
+        {18, 21, 23, 26, 30}
+    };
+    searchElement(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+}
