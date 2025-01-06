587aa1c732501f35a525d2587622ad3b7717264b
Ultron011 <ultroninfinity01@gmail.com>
2025-01-06 10:47:55 +0530

    BINARY TREE - POSTORDER TRAVERSAL

---
+++ BINARY TREE/TRAVERSAL/POSTORDER/postOrder.cpp
@@ -0,0 +1,64 @@
+/**
+ * Function to perform postorder traversal of a binary tree.
+ *
+ * Problem Statement:
+ * Given the root of a binary tree, return the postorder traversal of its nodes' values.
+ * Postorder traversal is a type of depth-first traversal where the nodes are recursively visited in the order: left subtree, right subtree, root node.
+ *
+ * Input:
+ * - TreeNode* root: A pointer to the root node of the binary tree.
+ *
+ * Output:
+ * - vector<int>: A vector containing the values of the nodes in postorder traversal order.
+ *
+ * Time Complexity:
+ * - O(n): Where 'n' is the number of nodes in the binary tree. Each node is visited exactly once.
+ *
+ * Space Complexity:
+ * - O(h): Where 'h' is the height of the binary tree. This space is used by the call stack during the recursion. In the worst case, the height of the tree could be 'n' (in case of a skewed tree), leading to O(n) space complexity. In the best case (balanced tree), the height would be log(n), leading to O(log(n)) space complexity.
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+void postorder(Node* root, vector<int> &arr) {
+    if (!root) return;
+
+    postorder(root->left, arr);
+    postorder(root->right, arr);
+    arr.push_back(root->data);
+}
+
+vector<int> postOrder(Node* root) {
+    vector<int> arr;
+    postorder(root, arr);
+    return arr;
+}
+
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    vector<int> result = postOrder(root);
+
+    cout << "Postorder Traversal: ";
+    for(int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+
+    return 0;
+}
