0452a89371c9b6266cd6cac37c3d6ce9d92a3857
Ultron011 <ultroninfinity01@gmail.com>
2025-01-12 13:34:41 +0530

    TIME TAKEN TO BURN A BINARY TREE

--- BINARY TREE/HARD/TIME TAKEN TO BURN BINARY TREE/timeTakenToBurn.cpp
+++ BINARY TREE/HARD/TIME TAKEN TO BURN BINARY TREE/timeTakenToBurn.cpp
@@ -26,3 +26,116 @@
  *        - Time 4: Node 3 burns.
  *        Therefore, the minimum time required to burn the entire binary tree is 4 units.
  */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+    Time complexity: 
+        O(N) where n is the number of nodes.
+
+    Space complexity: 
+        O(N), where n is the number of nodes.
+*/
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+
+
+class Solution {
+
+    int timeTaken(unordered_map<TreeNode*, TreeNode*> &mpp, TreeNode* target) {
+        queue<TreeNode* > q;
+        q.push(target);
+        unordered_map<TreeNode*, bool> visited;
+        visited[target] = true;
+
+        int time = 0;
+        while (!q.empty()) {
+            int size = q.size();
+
+            /* If any of the child or parent node is burned or not */
+            int fl = 0;
+            
+            for (int i = 0; i < size; i++) {
+                auto node = q.front();
+                q.pop();
+                
+                if (node->left && !visited[node->left]) {
+                    fl = 1;
+                    q.push(node->left);
+                    visited[node->left] = true;
+                }
+                if (node->right && !visited[node->right]) {
+                    fl = 1;
+                    q.push(node->right);
+                    visited[node->right] = true;
+                }
+                if (mpp[node] && !visited[mpp[node]]) {
+                    fl = 1;
+                    q.push(mpp[node]);
+                    visited[mpp[node]] = true;
+                }
+            }
+
+            // If any adjacent node burned, increase the time by 1
+            if (fl) time++;
+        }
+        return time;
+    }
+
+    TreeNode* bfsToMapParents(TreeNode* root, unordered_map<TreeNode*, TreeNode*> &mpp, int target) {
+        queue<TreeNode*> q;
+        q.push(root);
+        TreeNode* res;
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            if (node->val == target) {
+                res = node;
+            }
+            q.pop();
+            if(node->left) {
+                mpp[node->left] = node;
+                q.push(node->left);
+            }
+            if (node->right) {
+                mpp[node->right] = node;
+                q.push(node->right);
+            }
+        }
+        return res;
+    }
+
+public:
+    int minTimeToBurn(TreeNode* root, int target) {
+        unordered_map<TreeNode*, TreeNode*> mpp;
+        TreeNode* targetNode = bfsToMapParents(root, mpp, target);
+        int time = timeTaken(mpp, targetNode);
+        return time;
+    }
+};
+
+
+int main() {
+    TreeNode* root = new TreeNode(3);
+    root->left = new TreeNode(5);
+    root->right = new TreeNode(1);
+    root->left->left = new TreeNode(6);
+    root->left->right = new TreeNode(2);
+    root->right->left = new TreeNode(0);
+    root->right->right = new TreeNode(8);
+    root->left->right->left = new TreeNode(7);
+    root->left->right->right = new TreeNode(4);
+
+    int target = 5;
+
+    Solution sol;
+    int minTime = sol.minTimeToBurn(root, target);
+    cout << "Minimum Time taken to burn the entire tree is : " << minTime << endl;
+
+}
